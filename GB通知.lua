49e37b6067b6b0dda534cdf21dced691cf971907
xuaoxi9008 <3574769415@qq.com>
2026-04-24 12:56:52 +0800

    Add files via upload

---
+++ GB通知.lua
@@ -0,0 +1,24 @@
+local repo = 'https://raw.githubusercontent.com/DevSloPo/obsidian_UI/main/'
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local playerName = game:GetService("Players").LocalPlayer.Name
+Library:Notify({
+    Title = "XIAOXI HUB",
+    Description = "欢迎付费用户: " .. playerName .. " 丨 正在加载XIAOXI内脏与黑火药",
+    Time = 6
+})
+local startTime = tick()      
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xiaoxi9008/Server./refs/heads/main/RB-XIAOXIGB.lua'))()
+local endTime = tick()
+local loadTime = string.format("%.2f", endTime - startTime)
+Library:Notify({
+    Title = "XIAOXI HUB",
+    Description = "加载器加载完成！耗时: " .. loadTime .. "秒",
+    Time = 6
+})
+local Sound = Instance.new("Sound")
+Sound.SoundId = "rbxassetid://4989569018"
+Sound.Parent = game:GetService("SoundService")
+Sound.Volume = 5
+Sound:Play()
+Sound.Ended:Wait()
+Sound:Destroy()
